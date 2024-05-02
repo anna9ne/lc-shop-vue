4202ad8d5de008c9095ea92accb7c3d998d37ff3
anna <anna9ne@gmail.com>
2024-05-02 13:54:38 +0300

    21. Пагинация vue на странице с продуктами

--- src/views/product/Index.vue
+++ src/views/product/Index.vue
@@ -316,18 +316,34 @@
                   </div>
                 </div>
               </div>
-              <div class="row">
+              <div class="row" v-if="pagination.last_page > 1">
                 <div class="col-12 d-flex justify-content-center wow fadeInUp animated">
                   <ul class="pagination text-center">
-                    <li class="next"><a href="#0"><i class="flaticon-left-arrows"
-                                                     aria-hidden="true"></i> </a></li>
-                    <li><a href="#0">1</a></li>
-                    <li><a href="#0" class="active">2</a></li>
-                    <li><a href="#0">3</a></li>
-                    <li><a href="#0">...</a></li>
-                    <li><a href="#0">10</a></li>
-                    <li class="next"><a href="#0"><i class="flaticon-next-1"
-                                                     aria-hidden="true"></i> </a></li>
+                    <li v-if="pagination.current_page !== 1" class="next">
+                      <a @click.prevent="getProducts(pagination.current_page - 1)" href="#0"><i class="flaticon-left-arrows"
+                                                     aria-hidden="true"></i> </a>
+                    </li>
+                    <li v-for="link in pagination.links">
+                      <template v-if="Number(link.label) &&
+                      (pagination.current_page - link.label < 2 &&
+                      pagination.current_page - link.label > -2) ||
+                      Number(link.label) === 1 || Number(link.label) === pagination.last_page">
+                        <a @click.prevent="getProducts(link.label)" :class="link.active ? 'active' : ''" href="#0">{{ link.label }}</a>
+                      </template>
+                      <template v-if="Number(link.label) &&
+                      pagination.current_page !== 3 &&
+                      pagination.current_page - link.label === 2 ||
+                      Number(link.label) &&
+                      pagination.current_page !== pagination.last_page - 2 &&
+                      pagination.current_page - link.label === -2">
+                        <a>...</a>
+                      </template>
+                    </li>
+
+                    <li v-if="pagination.current_page !== pagination.last_page" class="next">
+                      <a @click.prevent="getProducts(pagination.current_page + 1)" href="#0"><i class="flaticon-next-1"
+                                                     aria-hidden="true"></i> </a>
+                    </li>
                   </ul>
                 </div>
               </div>
@@ -358,6 +374,7 @@ export default {
       colors: [],
       tags: [],
       prices: [],
+      pagination: [],
     }
   },
 
@@ -368,19 +385,7 @@ export default {
 
       this.prices = prices.replace(/[\s+]|[$]/g, '').split('-')
 
-      this.axios.post('http://localhost:8876/api/products', {
-        'categories': this.categories,
-        'colors': this.colors,
-        'tags': this.tags,
-        'prices': this.prices
-      })
-          .then( res => {
-            this.products = res.data.data
-            console.log(res);
-          })
-          .finally( v => {
-            $(document).trigger('changed')
-          })
+      this.getProducts()
     },
 
     addColor(id) {
@@ -403,10 +408,17 @@ export default {
       }
     },
 
-    getProducts() {
-      this.axios.post('http://localhost:8876/api/products')
+    getProducts(page = 1) {
+      this.axios.post('http://localhost:8876/api/products', {
+        'categories': this.categories,
+        'colors': this.colors,
+        'tags': this.tags,
+        'prices': this.prices,
+        'page': page,
+      })
           .then( res => {
             this.products = res.data.data
+            this.pagination = res.data.meta
             console.log(res);
           })
           .finally( v => {
